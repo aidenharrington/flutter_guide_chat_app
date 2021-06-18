90112ebab246f6dffdfcdadb1ef33d9033a8103a
Aiden Harrington <aidenkharrington@gmail.com>
2021-06-18 07:50:38 -0700

    scrapping due to firebase firestore outdated video

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,27 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
 
-  
-  }
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
 
+class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (ctx, index) => Container(
-          padding: EdgeInsets.all(8),
-          child: Text('This works'),
-        ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('chats/Rcp9vcUdOTNUTc8fXqwI/messages')
+            .snapshots(),
+        builder: (ctx, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+          if (streamSnapshot.hasError) {
+            return Text('Something went wrong');
+          }
+
+          if (streamSnapshot.connectionState == ConnectionState.waiting) {
+            return Text('loading');
+          }
+
+          if (streamSnapshot.data == null) {
+            return Text('No data');
+          }
+
+          return new ListView(
+            children: streamSnapshot.data == null
+                ? streamSnapshot.data.docs.map((DocumentSnapshot document) {
+                    Map<String, dynamic> data =
+                        document.data() as Map<String, dynamic>;
+                    return new ListTile(
+                      title: Text(data['yFx4kvHm7iRPORhrPQvU']),
+                    );
+                  }).toList()
+                : [Text('No Data')],
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {
-          
-        },
+        onPressed: () {},
       ),
     );
   }
